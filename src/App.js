0e8ca71fa6e788bca87d233018bedbf8152b4001
Irvan Ahmad Prasetya <irvan.ap1618@gmail.com>
2020-01-16 12:02:05 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Container, Navbar, Nav, Jumbotron, Row, Col, Image } from 'react-bootstrap';
+import {
+  Container, Navbar, Nav, Jumbotron, Row, Col, Image, Card, Button
+} from 'react-bootstrap';
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 /* style */
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,7 +16,7 @@ function App() {
     <div className="App">
       <Navbar expand="lg" className="custom-navbar" fixed="top">
         <Container>
-          <Navbar.Brand href='#'>IAP</Navbar.Brand>
+          <Navbar.Brand href='#'>Irvan Ahmad P</Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar-nav'/>
           <Navbar.Collapse id='navbar-nav'>
             <Nav className="justify-content-end" style={{ width: "100%" }}>
@@ -32,12 +35,12 @@ function App() {
       </Navbar>
       <Container fluid style={{padding : '0px 0px' }}>
         <Jumbotron id="home" className="d-flex flex-column align-items-center justify-content-center content-full-cover">
-          <h1>IAP</h1>
+          <h1>Irvan Ahmad P</h1>
           <h4><p>Professional Bio</p></h4>
         </Jumbotron>
-        <Jumbotron id="about" className="d-flex flex-column align-items-center justify-content-center content-full bg-white">
+        <Jumbotron id="about" className="d-flex flex-column align-items-center justify-content-center content-full bg-light">
           <Row>
-            <h2>IAP</h2>
+            <h2>Irvan Ahmad P</h2>
           </Row>
           <Row>
             <Image className="photo-profile" src={Avatar} roundedCircle/>
@@ -46,6 +49,20 @@ function App() {
             <h4 className="text-secondary">
               Web Developer
             </h4>
+          </Row>
+        </Jumbotron>
+        <Jumbotron id="contact" className="d-flex flex-column align-items-center justify-content-center content-full bg-dark">
+          <Row>
+            <Col>
+              <a className="btn btn-light btn-lg" href="https://www.linkedin.com/in/irvan-ahmad-prasetya-6306a8115/" role="button">
+                <FaLinkedin/>
+              </a>
+            </Col>
+            <Col>
+              <a className="btn btn-light btn-lg" href="https://github.com/IrvanAhmadPrasetya" role="button">
+                <FaGithub/>
+              </a>
+            </Col>
           </Row>
         </Jumbotron>
       </Container>
