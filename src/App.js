f4cbb5d8055586ea3ce03e415d565cbb6a4db246
Irvan Ahmad Prasetya <irvan.ap1618@gmail.com>
2020-02-18 15:48:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Container, Navbar, Nav, Jumbotron, Row, Image
+  Container, Navbar, Nav, Jumbotron, Row, Col, Image, Card
 } from 'react-bootstrap';
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 
@@ -24,6 +24,9 @@ function App() {
                 <Nav.Link href="#home">Home</Nav.Link>
               </Nav.Item>
               <Nav.Item>
+                <Nav.Link href="#portofolio">Portofolio</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
                 <Nav.Link href="#about">About</Nav.Link>
               </Nav.Item>
             </Nav>
@@ -34,6 +37,57 @@ function App() {
         <Jumbotron id="home" className="d-flex flex-column align-items-center justify-content-center content-full-cover">
           <h1>Irvan Ahmad P.</h1>
           <h4><p>Professional Bio</p></h4>
+        </Jumbotron>
+        <Jumbotron id="portofolio" className="d-flex flex-column align-items-center justify-content-center content-full bg-white">
+          <Row>
+            <h2>Portofolio</h2>
+          </Row>
+          <Container style={{marginTop : '25px'}}>
+            <Row>
+              <Col>
+                <Card className="card-wrap">
+                  <div className="image-protofolio-wrap">
+                    <Image variant="top" rounded className="img-portofolio" src="https://raw.githubusercontent.com/IrvanAhmadPrasetya/RBuilder/master/images/Screenshot%20-%20RBuilder.png" />
+                  </div>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card className="card-wrap">
+                  <div className="image-protofolio-wrap">
+                    <Image variant="top" rounded className="img-portofolio" src="https://raw.githubusercontent.com/IrvanAhmadPrasetya/RBuilder/master/images/Screenshot%20-%20RBuilder.png" />
+                  </div>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card className="card-wrap">
+                  <div className="image-protofolio-wrap">
+                    <Image variant="top" rounded className="img-portofolio" src="https://raw.githubusercontent.com/IrvanAhmadPrasetya/RBuilder/master/images/Screenshot%20-%20RBuilder.png" />
+                  </div>
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
         </Jumbotron>
         <Jumbotron id="about" className="d-flex flex-column align-items-center justify-content-center content-full bg-light">
           <Row>
