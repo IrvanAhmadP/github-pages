f629aa18eb5d8f5f18b91ab7c6f47cc231f6687a
Irvan Ahmad Prasetya <irvan.ap1618@gmail.com>
2021-05-31 11:20:58 +0700

    rm v2 url

--- src/App.js
+++ src/App.js
@@ -32,9 +32,6 @@ function App() {
               <Nav.Item>
                 <Nav.Link href="#about">About</Nav.Link>
               </Nav.Item>
-              <Nav.Item>
-                  <Nav.Link href="/">V2</Nav.Link>
-                </Nav.Item>
             </Nav>
           </Navbar.Collapse>
         </Container>
