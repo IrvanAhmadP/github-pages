9b87d268c36dcc46f40caec51f7b998733b65151
Irvan Ahmad Prasetya <irvan.ap1618@gmail.com>
2021-05-31 11:23:50 +0700

    update page

---
+++ build/precache-manifest.2f6aa2f34ad6f3b7f06b20c9fba2225a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc6bfc6d660923b3d73fa79f3ead4c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b5831e94035d6558dc7",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "cf1c9c139063dd6136af",
+    "url": "/static/css/main.efbd63cf.chunk.css"
+  },
+  {
+    "revision": "7b5831e94035d6558dc7",
+    "url": "/static/js/2.f5e46073.chunk.js"
+  },
+  {
+    "revision": "cf1c9c139063dd6136af",
+    "url": "/static/js/main.7f166d20.chunk.js"
+  },
+  {
+    "revision": "f0f30864e14c709c8a88",
+    "url": "/static/js/runtime-main.4bdb7193.js"
+  }
+]);
